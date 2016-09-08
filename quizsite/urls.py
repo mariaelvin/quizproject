47fc2f1a184a9a015cbebb79d2001a0e18efbcac
Maria Elvin <maria@tedgustaf.com>
2016-09-08 11:26:50 +0200

    fix i views.py och urls.py

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url
 from quiz import views
-
 urlpatterns = [
     url(r"^$", views.startpage),
     url(r"^quiz/[0-9]+/$",views.quiz),
