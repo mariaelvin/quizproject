25d5052a559e8069619662a2f5d513f666d2761f
Maria Elvin <maria@tedgustaf.com>
2016-09-08 12:11:54 +0200

    Lagt till quiz-data i quiz-template

--- quiz/views.py
+++ quiz/views.py
@@ -24,8 +24,12 @@ def startpage(request):
 	}
 	return render(request, "quiz/startpage.html", context)
 
-def quiz(request):
-	return render(request, "quiz/quiz.html")
+def quiz(request,quiz_number):
+	context = {
+	"quiz": quizzes[int(quiz_number)-1],
+	"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/quiz.html", context)
 
 def question(request):
 	return render(request, "quiz/question.html")
