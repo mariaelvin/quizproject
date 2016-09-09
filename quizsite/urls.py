795533f9e21b004c377feeab00a766da2f46ee80
Maria Elvin <maria@tedgustaf.com>
2016-09-09 10:25:54 +0200

    Lade till adminprylar till django

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from quiz import views
+from django.contrib import admin
+from django.conf.urls import include
+
 urlpatterns = [
     url(r"^$", views.startpage, name="start_page"),
     url(r"^quiz/([0-9]+)/$",views.quiz, name="quiz_page"),
     url(r"^quiz/([0-9]+)/question/([0-9]+)/$", views.question, name="question_page"),
 	url(r"^quiz/([0-9]+)/completed/$", views.completed, name="completed_page"),
+    url(r"^admin/", include(admin.site.urls)),
 ]
